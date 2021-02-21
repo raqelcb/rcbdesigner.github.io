47e60a2283c499e952aa257cd4583da6c412b94b
Raquel CB <raqelcb@me.com>
2021-02-21 20:37:39 +0100

    add .js menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 'use strict';
+var icono = document.querySelector('.navbar__fas');
+var nav = document.querySelector('.navbar__collapse');
+icono.addEventListener('click', function () {
+    nav.classList.toggle('ver');
+});
