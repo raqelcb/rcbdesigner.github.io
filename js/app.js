db940d45f8f768629f8221e48de98f9d693395ca
Raquel CB <raqelcb@me.com>
2021-02-22 04:00:21 +0100

    js modifications, delete typer

--- js/app.js
+++ js/app.js
@@ -81,8 +81,6 @@ var siguienteFoto = function () {
   });
 
 
-
-
 // Back to top button
 $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
@@ -99,14 +97,14 @@ $(window).scroll(function() {
   });
 
 
-/*--/ Star Typed /--*/
-if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-    var typed = new Typed('.text-slider', {
-      strings: typed_strings.split(','),
-      typeSpeed: 80,
-      loop: true,
-      backDelay: 1100,
-      backSpeed: 30
-    });
-  }
+// /*--/ Star Typed /--*/
+// if ($('.text-slider').length == 1) {
+//     var typed_strings = $('.text-slider-items').text();
+//     var typed = new Typed('.text-slider', {
+//       strings: typed_strings.split(','),
+//       typeSpeed: 80,
+//       loop: true,
+//       backDelay: 1100,
+//       backSpeed: 30
+//     });
+//   }
